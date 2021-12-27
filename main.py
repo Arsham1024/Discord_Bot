493e90e7f76fcfa1a2f39e5f59da388940ac9aff
Arsham Mehrani <Arsham512@gmail.com>
2021-12-26 16:06:10 -0800

    everyone's discord bot code is here now

--- main.py
+++ main.py
@@ -1,97 +1,147 @@
-# discord api
 import discord
-from discord.utils import get
 import os
-import asyncio
-# for working with apis 
-import requests
-import json
-# make a server that runs replit constantly
-from KeepAlive import keep_alive
-# database
+import random
+from keep_alive import keep_alive
 from replit import db
-import matplotlib.pyplot as plt
-
+from discord.ext import tasks
 
 client = discord.Client()
-sleeptime = 1 # change this to modify how long the bot waites before counting
+# used in dailyMessage(), determines how often to send a message
+if "freq" not in db.keys():
+    db["freq"] = 5
 
-def makeplot(names , values):
-  # make the plot and send it to the chat!
-  plt.bar(names, values)
-  plt.show()
-  plt.savefig(fname='./plooot.png')
-  
-  
 
-# returns a fortune from the online api
-def get_fortune():
-  response = requests.get('https://zenquotes.io/api/random').text
-  json_data = json.loads(response)
-  quote = json_data[0]['q'] + ' \n\t-' + json_data[0]['a']
-  return(quote)
+def updateFacts(fact):
+    if "facts" in db.keys():
+        facts = db["facts"]
+        facts.append(fact)
+        db["facts"] = facts
+    else:
+        db["facts"] = [fact]
+
+
+def deleteFacts(index):
+    index = int(index)
+    facts = db["facts"]
+    if len(facts) > index:
+        del facts[index]
+        db["facts"] = facts
+        return True
+
+
+def editFacts(index, edit):
+    index = int(index)
+    facts = db["facts"]
+    if len(facts) > index:
+        facts[index] = edit
+        return True
+
+
+def changeFrequency(freq):
+    db["freq"] = freq
+    dailyMessage.change_interval(seconds=db["freq"])
+
 
 @client.event
+# Display bot is logged on
 async def on_ready():
-  print(f'Hello {client.user}, I\'m online and ready!')
+    print("Logged in as {0.user}".format(client))
+    dailyMessage.start()
 
 
 @client.event
-async def on_message(message1):
-  # for parsing the message1
-  message = message1.content
-  channel = message1.channel
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-  # if the message is from the bot itself ignore
-  if message1.author == client.user:
-    return
-  # if message starts with $ it is a command
-  if message.startswith('!f'):
-    # return fortune
-    await channel.send(get_fortune())
+    msg = message.content
 
-  elif message.startswith('!p'):
-    pollmessage = message
-    parsed_msg = message.split('-')
-    prompt = parsed_msg[1]
-    options = parsed_msg[2:]
+    # Assume theres always at least one fact in database
+    # !fact - Display a random fact
+    if msg.startswith("!fact"):
+        await message.channel.send("```{}```".format(random.choice(db["facts"])))
 
-    # output prompt that user specified
-    await channel.send('taking a poll now!\n' + prompt)
-    #print out all the options that the user inputed
-    bot_msg = ''
-    for i in range(len(options)):
-      bot_msg += str("\n\t" + str(i+1) + f") {options[i]}")
+    # !add - Add a fact to the database
+    elif msg.startswith("!add"):
+        try:
+            fact = msg.split("!add ", 1)[1]
+            await message.channel.send("Fact added")
+            updateFacts(fact)
+        except IndexError:
+            await message.channel.send("Something went wrong, try again.")
 
-    # message to react to 
-    reactto = await channel.send(bot_msg)
-    # numbers for indications
-    emoji_numbers = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
-    # add reaction to own bot's message coresponding to the number of options
-    for i in range(len(options)):
-        await reactto.add_reaction(emoji_numbers[i])
+    # !delete - Delete a fact from the database
+    elif msg.startswith("!delete"):
+        try:
+            index = msg.split("!delete ", 1)[1]
+            facts = db["facts"]
+            if deleteFacts(index):
+                await message.channel.send("Fact deleted")
+            else:
+                await message.channel.send("Index out of range")
+        except IndexError:
+            await message.channel.send("Something went wrong, try again.")
 
-    await asyncio.sleep(sleeptime)  # wait for one minute
+    # !listall - lists all the facts form the database
+    elif msg.startswith("!listall"):
+        facts = db["facts"]
+        if len(facts) == 0:
+            await message.channel.send("```There are no facts. Add a fact using !add {string}```")
+        else:
+            for i in range(len(facts)):
+                await message.channel.send("```[{}] {}```".format(i, facts[i]))
 
-    cache_msg = discord.utils.get(client.cached_messages, id=reactto.id)
+    # !help - Display all comands
+    elif msg.startswith("!help"):
+        await message.channel.send("```!fact - Display a random fact from database \n" +
+                                   "!add {string} - Add a fact to the database \n" +
+                                   "!delete {index} - Delete selected index from database \n" +
+                                   "!listall - Lists all facts from the database \n" +
+                                   "!edit {index} {string} - Edit a specified index of the database. The string will completely override the original.\n" +
+                                   "!size - Displays the number of facts in the database \n" +
+                                   "!freq {int} - Changes the frequency of the daily messages (in seconds) \n" +
+                                   "!help - Display list of commands```")
 
-    # make names and values for plot device
-    names = []
-    values = []
-    print(cache_msg.reactions[:])
-    for i in cache_msg.reactions:
-      print(i.emoji , i.count)
-      names.append(i.emoji)
-      values.append(i.count)
-    
-    plt.bar(options , values)
-    plt.savefig("plot.png")
-    await channel.send(file=discord.File('plot.png'))
-    
+    # !edit - Overwrite a specified index
+    elif msg.startswith("!edit"):
+        index = msg.split("!edit ", 1)[1]
+        try:
+            edit = index.split(" ", 1)[1]
+            index = index.split(" ", 1)[0]
+        except IndexError:
+            await message.channel.send("Something went wrong, try again.")
+        else:
+            await message.channel.send("Editting Fact {}".format(index))
+            if (editFacts(index, edit)):
+                await message.channel.send("Editting successful")
+            else:
+                await message.channel.send("Index out of range")
 
-# use uptimerobot.com . keeps this script running all the time.
-# keep_alive()
+    # !size - Display how many facts are in the database
+    elif msg.startswith("!size"):
+        await message.channel.send("There are {} facts in my database".format(len(db["facts"])))
 
-# Environment variable is a variable that is private. because this repil is public
-# need to hide the TOKEN of the bot.
-client.run(os.environ['TOKEN'])
+    # !freq - Change how often facts are relayed
+    elif msg.startswith("!freq"):
+        try:
+            freq = msg.split("!freq ", 1)[1]
+            freq = int(freq.split(" ", 1)[0])
+        except IndexError:
+            await message.channel.send("Something went wrong, try again.")
+        else:
+            changeFrequency(freq)
+            await message.channel.send("Frequency of daily messages changed to: {} seconds".format(freq))
+
+
+# Display a random fact every frequency seconds
+@tasks.loop(seconds=db["freq"])
+async def dailyMessage():
+    print(db["freq"])
+    # CHANNELID - Channel Id contained in .env
+    channel = client.get_channel(int(os.getenv('CHANNELID')))
+    await channel.send("```{}```".format(random.choice(db["facts"])))
+
+
+# Webserver to keep bot constantly running
+keep_alive()
+client.run(os.getenv('TOKEN'))
